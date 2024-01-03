8735b19fab90cd7f9a200b4639c17d099e678d14
sudarsanpidugu <sudarsan2165@gamil.com>
2024-01-03 21:28:14 +0530

    new

--- repython.py
+++ repython.py
@@ -9,3 +9,92 @@ Mrs. Sravya has learned Oracle Databas from ABC Institute and placed in Wipro Co
 '''
 1.WAP to get all students names?
 '''
+
+
+'''
+2. WAP to get all male students names?
+'''
+
+'''
+3. WAP to get all female students name?
+'''
+
+
+'''
+4. WAP to get all married female students names?
+'''
+
+'''
+5. WAP to get all unmarried female students names?
+'''
+
+'''
+6. WAP to get all courses names? 
+'''
+
+'''
+7.WAP to get all languages names?
+'''
+
+'''
+8. WAP to get all databases names?
+'''
+
+'''
+9. WAP to get all institute names?
+'''
+
+
+'''
+10. WAP to get all companies names?
+'''
+
+'''
+11. WAP to get all salaries?
+'''
+
+'''
+12. WAP to get language name of Mr.Kumar?
+'''
+
+
+'''
+13. WAP to get company name of Mr. Suresh?
+'''
+
+
+'''
+# 14. WAP to get both institute name and company name of Ms. Kavaya?
+
+'''
+
+
+'''
+15. WAP to get language name and salary of Mr. Suresh?
+'''
+
+
+
+'''
+16. WAP to get all details  of MS. Bavya?
+'''
+
+
+'''
+17.WAP to get all students names who learned course from NTH Institute?
+'''
+
+
+'''
+18. WAP to get all students names who learned languges?
+'''
+
+
+'''
+19. WAP to get all students names who learned databases?
+'''
+
+
+'''
+20. WAP to get all  
+'''
