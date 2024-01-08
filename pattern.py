585b4a077eb3d66fbe33a0df56ef85c0e4a89954
sudarsanpidugu <sudarsan2165@gamil.com>
2024-01-08 14:16:55 +0530

    add git code

---
+++ pattern.py
@@ -0,0 +1,64 @@
+# 1. pattern - 1?
+'''
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+'''
+# n = eval(input('Enter Any Number: '))
+
+# for i in range(1, n+1):
+#     for j in range(1,n+1):
+#         print('*', end=' ')
+#     print()
+
+# 2. pattern -2?
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+'''
+
+# n = eval(input('Enter Any Name: '))
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print('*', end=' ')
+#     print()
+    
+# 3.pattern -3?
+'''
+* * * * *
+* * * *
+* * *
+* *
+*
+'''
+# n = eval(input('Enter Any Number: '))
+
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print('*', end=' ')
+#     print()
+
+# 4. pattern -4?
+'''
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+'''
+
+# n = eval(input('Enter Any Number: '))
+# for i in range(1,n+1):
+#     for j in range(1,n+1-i):
+#         print(' ', end=' ')
+#     for k in range(1,i+1):
+#         print('*' , end=' ')
+#     print()
+
+5. 
--- pythonfile.py
+++
@@ -1,2 +0,0 @@
-# x = open('pavan.txt','w').write('TCS,Sai,Rohit,Satya,Dhoni,Sarath,Saroj,Venkat,Sas\nINFOSYS,Kohli,Santosh,Venkat,Koti,Prabha,Soumya,Mishra\nWIPRO,Satya,Kohli,Ram,Chinna,Pop,Amelia,Suresh,Arjuna')
-# print(x)
